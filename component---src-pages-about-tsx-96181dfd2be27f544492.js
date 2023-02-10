e1a1593b6a3a5af3fd931017a444808370b95496
metacode22 <alohajune22@gmail.com>
2023-02-10 20:36:58 +0900

    Updates

--- component---src-pages-about-tsx-96181dfd2be27f544492.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[49],{2766:function(e,l,s){s.r(l);var u=s(5893);l.default=()=>(0,u.jsx)("div",{children:"열띠미 만드는 중"})}}]);
-//# sourceMappingURL=component---src-pages-about-tsx-96181dfd2be27f544492.js.map
