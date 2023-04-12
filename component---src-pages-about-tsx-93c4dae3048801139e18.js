43b91c9097b509b1b19f7b5215c379ba52948f0f
metacode22 <alohajune22@gmail.com>
2023-04-12 19:01:30 +0900

    Updates

---
+++ component---src-pages-about-tsx-93c4dae3048801139e18.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkblog=self.webpackChunkblog||[]).push([[49],{2766:function(e,t,i){i.r(t);var l=i(6732),a=i(6267),r=i(5893);t.default=e=>{let{location:{href:t},data:{site:{siteMetadata:{title:i,description:l}},file:{publicURL:c}}}=e;return(0,r.jsx)(a.Z,{title:"About",description:l,url:t,image:c,children:(0,r.jsx)(s,{children:"열띠미 만드는 중!"})})};const s=(0,l.Z)("div",{target:"eexpkme0"})("")}}]);
+//# sourceMappingURL=component---src-pages-about-tsx-93c4dae3048801139e18.js.map
