1c0bfed8ab71b6cae6dddcbb3d379c99982046cf
metacode22 <alohajune22@gmail.com>
2023-06-02 17:33:14 +0900

    chore: font 파일들을 assets 폴더에서 static 폴더로 이동

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,7 +17,6 @@ exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
     output,
     resolve: {
       alias: {
-        assets: path.resolve(__dirname, 'src/assets'),
         components: path.resolve(__dirname, 'src/components'),
         hooks: path.resolve(__dirname, 'src/hooks'),
         pages: path.resolve(__dirname, 'src/pages'),
